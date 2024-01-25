bd22cb45e22eaea33c3d6d6651e80f704beb6c6a
Sinjarr <155503186+Sinjarr@users.noreply.github.com>
2024-01-25 20:31:18 +0100

    Update GG.java

--- GG.java
+++ GG.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class Main {
   public static void main(String[] args) {
-    Pattern pattern = Pattern.compile("wscooo", Pattern.CASE_INSENSITIVE);
+    Pattern pattern = Pattern.compile("wscooop", Pattern.CASE_INSENSITIVE);
     Matcher matcher = pattern.matcher("Visi WeSs!");
     boolean matchFound = matcher.find();
     if(matchFound) {
