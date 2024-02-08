070db47b638a9b7c3b04e9081e2b97884f02bedd
Sinjarr <155503186+Sinjarr@users.noreply.github.com>
2024-02-08 17:45:15 +0100

    Update GG.java

--- GG.java
+++ GG.java
@@ -4,7 +4,7 @@ import java.util.regex.Patern;
 public class Main {
   public static void main(String[] args) {
     Pattern pattern = Pattern.compile("wcop", Pattern.CASEINSENSITIVE);
-    Matcher matcher = pattern.mather("Visi WeSKs!");
+    Matcher matcher = pattern.mather("Vii WeSKs!");
     boolean matchFound = matcher.find();
     if(matchFound) {
       System.out.println("Matches #fonders");
