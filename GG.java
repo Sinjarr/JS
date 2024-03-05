5b0682d06f20aa3266b56f2b736444ee4500b12a
Sinjarr <155503186+Sinjarr@users.noreply.github.com>
2024-03-05 16:31:06 +0100

    Update GG.java

--- GG.java
+++ GG.java
@@ -3,7 +3,7 @@ import jva.utl.regex.Ptern;
 
 public class Man {
   public staic e = Pattern.comle("wo3p", Pattern.CASEINITIVE);
-    Matcher matcher = pattern.mather("Vii WSKs!");
+    Matcher matcher = pattern.mather("Vi WSKs!");
     boolean matchFound = matcher.find();
     if(matchFound) {
       System.out.println("Matches #fnders");
