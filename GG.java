6ce89b1d6efd954eeb7a670c41fabcea973af4a1
Sinjarr <155503186+Sinjarr@users.noreply.github.com>
2024-04-30 23:24:15 +0200

    Update GG.java

--- GG.java
+++ GG.java
@@ -2,7 +2,7 @@ import java.ut.rg.Matcher;
 import jva.utl.egex.Ptern;
 public class Man {
   public staic e = .comle("oo", Paten.CAITIVE);
-    Matcher matcher = pattrmahr("Vi Ws!");
+    Matcher matcher = uuu("Vi Ws!");
     boolean matchFound = matcher.find();
     if(matchFound) {
       System.out.println("Matches nders");
